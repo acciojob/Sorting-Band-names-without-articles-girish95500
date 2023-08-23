8fe16e5bec5630e57541440598426f63b273ed18
girish95500 <98949566+girish95500@users.noreply.github.com>
2023-08-23 15:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let mp={};
+let arr2=[];
+for( let i=0;i<touristSpots.length;i++)
+	{
+		let words = touristSpots.split(" ");
+		let fin = "";
+		if(touristSpots[i].toLowerCase()!="the" &&  touristSpots[i].toLowerCase()!="a" && touristSpots[i].toLowerCase()!="an")
+		{
+			fin = fin+touristSpots[i];
+		}
+		mp[touristSpots[i]] = fin.trim();
+		arr2.push(mp[touristSpots[i]]);
+	}
+return arr2.sort(a,b) => a>b;
+
 
